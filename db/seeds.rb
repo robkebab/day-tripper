69e64fbc59ee685722db021210af9ce9ea5924d7
Tr1ckC0 <trick.co@yahoo.com>
2020-04-14 14:24:08 -0500

    added user data to seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times {User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.first_name, email: Faker::Internet.email, password_digest: "12345")}
