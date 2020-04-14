c1c71041df5d41dd7e6adf7a1d1b2f107ab12a60
Tr1ckC0 <trick.co@yahoo.com>
2020-04-14 15:03:22 -0500

    added associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+#===================== ASSOCIATIONS ===============================================
+    has_many :driver_relationships
+    has_many :driver_trips, through: :driver_relationships, source: :trip
+    has_many :passenger_relationships
+    has_many :passenger_trips, through: :passenger_relationships, source: :trip
+
+    has_many :cars
+
+#========================= Validations ==============================================
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :password_digest, presence: true
 end
