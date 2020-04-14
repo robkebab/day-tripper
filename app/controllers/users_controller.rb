f3746a65fad568c1e30127419d841fd4ee6724bd
Tr1ckC0 <trick.co@yahoo.com>
2020-04-14 15:31:03 -0500

    added users#new users#create -flash errors needed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            # flash[:message] = "Successfully Created your Account" research flash messages
+            redirect_to @user
+        else
+            # flash[:error] = "You suck"
+            render "new"
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password_digest)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'welcome/index'
+
+  #trips
   resources :trips
+
+  #locations
   resources :locations
-  resources :users
+
+  #users
+  get '/signup', to: 'users#new', as: 'signup'
+  resources :users, except: [:new]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
